9082236096202c24ec2e6c35f246c8b599835cb6
Pratul Kumar <pratulkumar2003@gmail.com>
2026-04-11 18:13:15 +0530

    Views Updated for User app

---
+++ backend/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import UserListView, UserRegisterView
+
+urlpatterns=[
+    path('list/', UserListView.as_view(), name='users-list'),
+    path('register/', UserRegisterView.as_view(), name='register-user')
+]
